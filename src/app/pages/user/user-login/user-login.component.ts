6fe56d829e627edb5e09fbc098e80e2d3e7eb770
Rafael Tabolka <rafaeltabolka@hotmail.com>
2024-12-05 14:52:22 -0300

    Lógica prévia finalizada. Botões desabilitam com formulário não preenchido

--- src/app/pages/user/user-login/user-login.component.ts
+++ src/app/pages/user/user-login/user-login.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NavBarService } from '../../../services/nav-bar.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
 export class UserLoginComponent {
   formLogin: FormGroup;
-  // haveLogin:boolean = false;
-  // haveNotLogin:boolean = true;
+  showMessageError: boolean = false;
   
   constructor (
     private fb: FormBuilder,
@@ -31,14 +31,18 @@ export class UserLoginComponent {
   }
 
   onSubmit() {
-    if(true
+    if(this.formLogin.value.email == "fatec@fatec.sp.gov.br" &&
+    this.formLogin.value.password == "fatec123"
     ) {
-      // this.haveLogin = !this.haveLogin
-      // this.haveNotLogin = !this.haveNotLogin
+      console.log(this.formLogin.value.email);
+      
       this.navBarService.modifyStateLogin();
-      localStorage.setItem('haveLogin', JSON.stringify(this.navBarService.haveLogin))
-      localStorage.setItem('haveNotLogin', JSON.stringify(this.navBarService.haveNotLogin))
-      this.router.navigateByUrl('/home')
+      localStorage.setItem('haveLogin', JSON.stringify(this.navBarService.haveLogin));
+      localStorage.setItem('haveNotLogin', JSON.stringify(this.navBarService.haveNotLogin));
+      this.router.navigateByUrl('/home');
+      this.showMessageError = false;
+    }else {
+      this.showMessageError = true;
     }
   }
 }
--- src/app/pages/user/user-register/user-register.component.ts
+++ src/app/pages/user/user-register/user-register.component.ts
@@ -1,28 +1,36 @@
 import { Component } from '@angular/core';
 import { NavBarService } from '../../../services/nav-bar.service';
 import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './user-register.component.html',
   styleUrl: './user-register.component.css'
 })
 export class UserRegisterComponent {
+  formRegister: FormGroup;
   
   constructor(
     private navBarService: NavBarService,
-    private router: Router
-  ){}
+    private router: Router,
+    private fb: FormBuilder) {
+    this.formRegister = this.fb.group({
+      completeName:['', Validators.required],
+      email:['', [Validators.required, Validators.email]],
+      cpf:['', Validators.required],
+      phone:['', Validators.required],
+      password:['', Validators.required],
+      confirmPassword:['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {}
 
   onSubmit() {
-    if(true
-    ) {
-      // this.haveLogin = !this.haveLogin
-      // this.haveNotLogin = !this.haveNotLogin
+    if(this.formRegister.valid) {
       this.navBarService.modifyStateLogin();
       localStorage.setItem('haveLogin', JSON.stringify(this.navBarService.haveLogin))
       localStorage.setItem('haveNotLogin', JSON.stringify(this.navBarService.haveNotLogin))
