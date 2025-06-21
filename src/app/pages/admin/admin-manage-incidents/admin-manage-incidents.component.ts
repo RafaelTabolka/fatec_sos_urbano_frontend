339e0393121d3cffa06ff45d891ba241cea585aa
Rafael <rafaeltabolka@hotmail.com>
2025-06-21 02:53:53 -0300

    Criado tela para admin visualizar usuários

--- src/app/pages/admin/admin-manage-incidents/admin-manage-incidents.component.ts
+++ src/app/pages/admin/admin-manage-incidents/admin-manage-incidents.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarAdminComponent } from "../../../components/sidebar-admin/sidebar-admin.component";
+import { RevealOnScrollDirective } from '../../../shared/reveal-on-scroll.directive';
 
 @Component({
   selector: 'app-admin-manage-incidents',
   standalone: true,
-  imports: [SidebarAdminComponent],
+  imports: [RevealOnScrollDirective],
   templateUrl: './admin-manage-incidents.component.html',
   styleUrl: './admin-manage-incidents.component.css'
 })
